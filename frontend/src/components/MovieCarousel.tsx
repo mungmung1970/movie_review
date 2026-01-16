214f126e5911ebb0c95f0c5818496ac55e10287e
오민경 <mungmung1970@gmail.com>
2026-01-16 15:31:19 +0900

    carousel 높이 조정

--- frontend/src/components/MovieCarousel.tsx
+++ frontend/src/components/MovieCarousel.tsx
@@ -2,6 +2,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import "./MovieCarousel.css";
+
 
 import type { Movie } from "../types/movie";
 import MovieCard from "./MovieCard";
